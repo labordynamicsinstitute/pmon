4b15ce69d43e574d22bd41eaf3acbc9739966bbc
Lars Vilhuber <lars.vilhuber@cornell.edu>
2009-01-19 21:28:59 +0000

    Start of multi-server changes - some cleanup (Lars preferences)

---
+++ monitor/run/db/load_jobs.sql
@@ -0,0 +1,12 @@
+-- *****************************************************
+-- $Id$
+-- $URL$
+-- SQLite Code to load example jobs into the  pmon  db
+-- Usage:
+-- $ sqlite3 dbname < load_jobs.sql
+-- *****************************************************
+INSERT into jobs (dlabel) VALUES ("n111");
+INSERT into jobs (dlabel) VALUES ("n222");
+INSERT into jobs (dlabel) VALUES ("n333");
+INSERT into jobs (dlabel) VALUES ("n444");
+INSERT into jobs (dlabel) VALUES ("n555");
