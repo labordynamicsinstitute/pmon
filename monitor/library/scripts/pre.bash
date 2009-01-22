0d01d08153c4f01860705f264d5152fe010a1c89
Lars Vilhuber <lars.vilhuber@cornell.edu>
2009-01-22 18:23:16 +0000

    Fixed a condition where the total number of jobs might be negative, confusing some SQL queries and leading to (temporary) overloading of compute node

--- monitor/library/scripts/pre.bash
+++ monitor/library/scripts/pre.bash
@@ -9,4 +9,11 @@ echo "PID= " $$
 #mkdir -p /temporary/saswork2/tr0612mckin013/daemon/${dlabel}
 # Write out PID file
 #echo "0" > ${cdir}/run/pid/pid_pre_$$
+      ran_range=30
+      slptime=61
+      rannum=$RANDOM
+      let "rannum %= $dran_range"
+      let "slptime -= $ran_range"
+      let "slptime += $rannum"
+
 echo "0" > ${cdir}/run/$(hostname)/pid/pid_pre_$$
