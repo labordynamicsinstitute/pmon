29ede67ec20f103e27ac567c05b40988aab6ed75
Lars Vilhuber <lars.vilhuber@cornell.edu>
2009-02-18 14:12:55 +0000

    Clarified documentation in the bash scripts, highlighting the compulsory update to the PID file

--- monitor/library/scripts/main.bash
+++ monitor/library/scripts/main.bash
@@ -1,18 +1,41 @@
 #!/bin/bash
-# rargs gives the 4-x args
+# Structure of a pmon script
+#
+# These arguments are always available to the script when 
+# called. Any other arguments needed by the script would
+# need to be passed through by a file.
+# 
 jdir=$1
 dlabel=$2
 cdir=$3
 
-echo "jdir= " $jdir
-echo "dlabel= " $dlabel
-echo "PID= " $$
+# Some derived variables. Note that these should be
+# kept for later compulsory processing.
+sect=$(basename $0 .bash)
+pid=$$
+# The use of the PID file is COMPULSORY
+pid_file=${cdir}/run/$(hostname)/pid/pid_${sect}_${pid}
+# The use of a log file is OPTIONAL. Note that all
+# screen output from this script is already captured to a log file
 
-#Update PID file
-echo "2" > ${cdir}/run/$(hostname)/pid/pid_main_$$
-# Start SAS job
-#/usr/local/bin/sas ${cdir}/library/sasprogs/fcreate.sas -set dlabel $dlabel -set work /temporary/saswork2/tr0612mckin013/daemon/${dlabel} -log ${jdir}/${dlabel}/fcreate.log -print ${jdir}/${dlabel}/fcreate.lst
-echo "$(date) Starting job" > ${jdir}/${dlabel}/mylog
-sleep 60
-echo "$(date) Ending job" >> ${jdir}/${dlabel}/mylog
-echo "${?}" > ${cdir}/run/$(hostname)/pid/pid_main_$$
+#=================================================================
+# The following section is where the work is done. Insert here
+# whatever you need
+# 
+echo "$(date) Starting job" 
+echo "jdir= " $jdir
+echo "dlabel= " $dlabel "(state)"
+echo "PID= " $pid
+
+#Update PID file. This is optional, but see the end of the script.
+echo "2" > $pid_file
+
+echo "$(date) Ending job" 
+#=================================================================
+# 
+# The following line is COMPULSORY. Unless pmon finds
+# a ZERO ('0') in the pid_file, it will consider the job
+# failed.
+#
+
+echo "0" > $pid_file
