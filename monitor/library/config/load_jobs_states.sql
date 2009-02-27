aab74e0866b0f725f7e1e63a4c58043e1c4f911d
Lars Vilhuber <lars.vilhuber@cornell.edu>
2009-02-27 05:30:16 +0000

    Added a sample job to load the pmon db with state-named jobs

---
+++ monitor/library/config/load_jobs_states.sql
@@ -0,0 +1,51 @@
+-- *****************************************************
+-- $Id: load_jobs.sql 165 2009-01-21 02:06:37Z vilhu001 $
+-- $URL: https://repository.vrdc.cornell.edu/VRDC/tools/branches/pmon-multi-server/monitor/library/config/load_jobs.sql $
+-- SQLite Code to load example jobs with state names into the  pmon  db
+-- Usage:
+-- $ sqlite3 dbname < load_jobs.sql
+-- *****************************************************
+INSERT into jobs (dlabel) VALUES ("ak");
+INSERT into jobs (dlabel) VALUES ("al");
+INSERT into jobs (dlabel) VALUES ("ar");
+INSERT into jobs (dlabel) VALUES ("az");
+INSERT into jobs (dlabel) VALUES ("ca");
+INSERT into jobs (dlabel) VALUES ("co");
+INSERT into jobs (dlabel) VALUES ("de");
+INSERT into jobs (dlabel) VALUES ("fl");
+INSERT into jobs (dlabel) VALUES ("ga");
+INSERT into jobs (dlabel) VALUES ("hi");
+INSERT into jobs (dlabel) VALUES ("ia");
+INSERT into jobs (dlabel) VALUES ("id");
+INSERT into jobs (dlabel) VALUES ("il");
+INSERT into jobs (dlabel) VALUES ("in");
+INSERT into jobs (dlabel) VALUES ("ks");
+INSERT into jobs (dlabel) VALUES ("ky");
+INSERT into jobs (dlabel) VALUES ("la");
+INSERT into jobs (dlabel) VALUES ("md");
+INSERT into jobs (dlabel) VALUES ("me");
+INSERT into jobs (dlabel) VALUES ("mi");
+INSERT into jobs (dlabel) VALUES ("mn");
+INSERT into jobs (dlabel) VALUES ("mo");
+INSERT into jobs (dlabel) VALUES ("ms");
+INSERT into jobs (dlabel) VALUES ("mt");
+INSERT into jobs (dlabel) VALUES ("nc");
+INSERT into jobs (dlabel) VALUES ("nd");
+INSERT into jobs (dlabel) VALUES ("nj");
+INSERT into jobs (dlabel) VALUES ("nm");
+INSERT into jobs (dlabel) VALUES ("nv");
+INSERT into jobs (dlabel) VALUES ("ok");
+INSERT into jobs (dlabel) VALUES ("or");
+INSERT into jobs (dlabel) VALUES ("pa");
+INSERT into jobs (dlabel) VALUES ("ri");
+INSERT into jobs (dlabel) VALUES ("sc");
+INSERT into jobs (dlabel) VALUES ("tn");
+INSERT into jobs (dlabel) VALUES ("tx");
+INSERT into jobs (dlabel) VALUES ("ut");
+INSERT into jobs (dlabel) VALUES ("va");
+INSERT into jobs (dlabel) VALUES ("vt");
+INSERT into jobs (dlabel) VALUES ("wa");
+INSERT into jobs (dlabel) VALUES ("wi");
+INSERT into jobs (dlabel) VALUES ("wv");
+INSERT into jobs (dlabel) VALUES ("wy");
+
